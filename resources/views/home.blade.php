857f8071f93765d63a08d45badbe5f47c7584d6c
Anushka <anushka@fairshare.tech>
2024-08-19 09:46:53 +0530

    Home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,124 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        
+    </script>
+        <title>Meera Overseas</title>
+        <style>
+             img {
+                width: 100%;
+                height: auto; /* Maintain aspect ratio */
+            }
+            h1{
+                margin : 30px
+            }
+            p{
+                margin : 30px;
+                font-family: serif;
+            }
+            .contact{
+                background-image: url('contactback.jpg');
+                color:white;
+                margin-left: 35px; 
+                margin-right: 35px; 
+                margin-bottom: 0;
+                font-size:18;
+            }
+            .footer{
+                background-color:#004000;
+                margin-top:0;
+                margin-left: 30px; 
+                margin-right:30px; 
+                text-align:center;
+            }
+            .whatsapp-link {
+                position: fixed;
+                bottom: 20px; /* Adjust as needed */
+                right: 20px; /* Adjust as needed */
+                z-index: 9999; /* Ensure it stays on top of other content */
+            }
+            .whatsapp-logo {
+                width: 70px; /* Adjust size as needed */
+                height: auto; /* Maintain aspect ratio */
+                border-radius: 50%; /* For circular shape */
+                /* Add any additional styling as needed */
+            }
+            h3{
+                text-align:center;
+            }
+            
+
+            
+        </style>
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:green;  margin:30px">
+    <div class="container justify-content-end">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="container">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  active" href="/aboutus">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/products">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contactus">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</nav>
+        <div class="home container ">
+            <img src="Meera_Overseas.png" alt="cover1" class="img-fluid">
+        </div>
+        <div class="container">
+        <h1> About Us</h1>
+        <p> Meera  Overseas, Established in the year 2024 is a registered company under MSME, Maharashtra Chamber of commerce, primarily as a Merchant Export Company in Nashik Maharashtra (India).We take immense pride in being your premier partner in the global Agriculture product export industry. With a relentless commitment to quality, sustainability, and excellence, we have established ourselves as a trusted name in the field, delivering premium agricultural products to discerning customers worldwide.
+        We specializes in the export of a diverse range of products, catering to various industries including Fruits, Vegetables. Our wide portfolio includes high-quality goods sourced from reliable suppliers, ensuring that our clients receive only the best products on the market.
+        </p>
+        </div>
+        <a href="https://wa.me/message/RCTMOKYQFGUKN1" class="whatsapp-link">
+            <img src="whatsapp.jpg" alt="WhatsApp Logo" class="whatsapp-logo">
+        </a>
+        <div class="contact" >
+            <h1>Get in Touch</h1>
+            <div class="row" style="margin:20px">
+            <div class="col-4">
+           <i class="fas fa-map-marker-alt fa-3x d-flex flex-column align-items-center "></i> <br>
+           <h3>Meera Overseas, Nashik</h3>
+            </div>
+            
+            <div class="col-4">
+            <i class="fas fa-phone fa-3x d-flex flex-column align-items-center"></i> <br>
+            <h3> 9876543210</h3>
+            
+            </div>
+            <div class="col-4">
+           <i class="fas fa-envelope icon fa-3x d-flex flex-column align-items-center"></i><br>
+           <h3> meeraoverseas@gmail.com<h3>
+            </div>
+        </div>
+           <div class="container">
+        <p class=" text-center text-white">MEERA OVERSEAS © 2024 All rights reserved</p>
+        </div>
+        </div>
+        <div class="container footer">
+       
+        <p ><a href="mailto:anushkagp21@gmail.com" style="color: white; text-right">Designed and Developed by Anushka Pachpute</a></p>
+    </div>
+    </body>
+</html>
